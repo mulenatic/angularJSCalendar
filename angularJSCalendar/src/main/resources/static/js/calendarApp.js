eeffcb682ba84602141aebd01d542826226854cf
Thomas Kaczmarek <mulenatic@gmail.com>
2016-09-07 20:02:49 +0200

    Providing dummy daysWithTasks from within the controller.

--- angularJSCalendar/src/main/resources/static/js/calendarApp.js
+++ angularJSCalendar/src/main/resources/static/js/calendarApp.js
@@ -8,4 +8,12 @@ calendarApp.controller("calendarCtrl", function($scope){
 	var now = new Date();
 	$scope.month = monthNames[now.getMonth()];
 	$scope.year = now.getFullYear();
+	
+	var daysWithTasks = [];
+	for ( i = 1; i < 32; i++ ) {
+		daysWithTasks.push( { day: i, 
+			tasks: [{ name: "Projekt 1", duration: "4" }, 
+			        { name: "Projekt 2", duration: "2"} ]});
+	}
+	$scope.daysWithTasks = daysWithTasks;
 });
